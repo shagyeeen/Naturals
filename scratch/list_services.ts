85cdf629a37d36e87ad17afe62e2c93aabf78643
shagyeeen <shagyeeen@gmail.com>
2026-05-16 05:17:27 +0530

    Fix: Remove scratch folder to resolve build error

--- scratch/list_services.ts
+++
@@ -1,8 +0,0 @@
-import { supabaseAdmin } from './src/lib/supabase-admin';
-
-async function listServices() {
-  const { data } = await supabaseAdmin.from('services').select('name').eq('is_active', true);
-  console.log(JSON.stringify(data, null, 2));
-}
-
-listServices();
